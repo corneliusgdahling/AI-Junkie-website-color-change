d72a451b9a5788e8a94a6061b4d302e11087b988
corneliusgdahling <corneliusgdahling@gmail.com>
2017-11-13 13:04:12 +0100

    Simple plain javascript to change colors to more readable

---
+++ re_color.js
@@ -0,0 +1,23 @@
+document.body.style.backgroundColor = "white"
+for (i = 0; i < document.getElementsByTagName('p').length; i++) {
+	document.getElementsByTagName('p')[i].style.color = "black";
+}
+for (i = 0; i < document.getElementsByTagName('b').length; i++) {
+	document.getElementsByTagName('b')[i].style.color = "black";
+}
+for (i = 0; i < document.getElementsByTagName('h1').length; i++) {
+	document.getElementsByTagName('h1')[i].style.color = "black";
+}
+for (i = 0; i < document.getElementsByTagName('h2').length; i++) {
+	document.getElementsByTagName('h2')[i].style.color = "black";
+}
+for (i = 0; i < document.getElementsByTagName('h3').length; i++) {
+	document.getElementsByTagName('h3')[i].style.color = "black";
+}
+for (i = 0; i < document.getElementsByTagName('font').length; i++) {
+	document.getElementsByTagName('font')[i].style.color = "black";
+}
+for (i = 0; i < document.getElementsByTagName('table').length; i++) {
+	document.getElementsByTagName('table')[i].style.color = "#FFFFCC";
+	document.getElementsByTagName('table')[i].style.backgroundColor = "#FFFFCC";
+}
